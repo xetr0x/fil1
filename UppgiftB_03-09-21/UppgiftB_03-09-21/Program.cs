b84a4013a735f0da72f4b0db84389c1d5129e9b1
kevin wilusz <kwilusz10@gmail.com>
2021-09-06 12:14:49 +0200

    jag testar bara githubs push.

--- UppgiftB_03-09-21/UppgiftB_03-09-21/Program.cs
+++ UppgiftB_03-09-21/UppgiftB_03-09-21/Program.cs
@@ -6,10 +6,12 @@ namespace UppgiftB_03_09_21
     {
         static void Main(string[] args)
         {
+
+            string x = 220;
             string citat = Console.ReadLine();
             char[] separators = new char[] { ' ', '.' };
 
-            string[] subs = string.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            string[] subs = string.Split(separators, StringSplitOptions.RemoveEmptyEntries); #yeeessssirrrrr
 
             foreach (var sub in subs)
             {
